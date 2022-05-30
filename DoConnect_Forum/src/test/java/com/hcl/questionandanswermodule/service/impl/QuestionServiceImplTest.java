d96f26f66a654bc9b8c75ca51f849aef92a3562c
DungDo <ducdung2098@gmail.com>
2022-05-30 12:01:29 +0700

    update testing

---
+++ DoConnect_Forum/src/test/java/com/hcl/questionandanswermodule/service/impl/QuestionServiceImplTest.java
@@ -0,0 +1,53 @@
+package com.hcl.questionandanswermodule.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+class QuestionServiceImplTest {
+    @Test
+    void save() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findByTopicId() {
+    }
+
+    @Test
+    void searchByTitle() {
+    }
+
+    @Test
+    void approvedQuestion() {
+    }
+
+    @Test
+    void findQuestionToApprove() {
+    }
+
+    @Test
+    void findAllPageNumber() {
+    }
+
+    @Test
+    void testFindAll() {
+    }
+
+    @Test
+    void toDtoList() {
+    }
+}
---
+++ DoConnect_Forum/src/test/java/com/hcl/questionandanswermodule/service/impl/UserServiceImplTest.java
@@ -0,0 +1,40 @@
+package com.hcl.questionandanswermodule.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceImplTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findByUsername() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void sendEmail() {
+    }
+
+    @Test
+    void toUserDTOList() {
+    }
+}
